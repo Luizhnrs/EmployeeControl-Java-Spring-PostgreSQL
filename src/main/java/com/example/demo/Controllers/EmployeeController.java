5c82d2f141f22d3e49557949ec7002ea26120805
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-16 15:29:14 -0300

    GetMapping

---
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@GetMapping("Employees")
+public class EmployeeController
+{
+    public void GetAll()
+    {
+
+    }
+}
