0258e82679803d349cafdbd0a86c30b7833c9a4a
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-16 17:45:56 -0300

    EmployeeList

--- src/main/java/com/example/demo/Employees/Employee.java
+++ src/main/java/com/example/demo/Employees/Employee.java
@@ -1,12 +1,12 @@
 package com.example.demo.Employees;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Table(name = "Employees")
 @Entity(name = "Employees")
 public class Employee
 {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String photo;
