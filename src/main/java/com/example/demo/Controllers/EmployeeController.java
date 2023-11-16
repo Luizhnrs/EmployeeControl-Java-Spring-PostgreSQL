3514169acb22a111257594c74ba1a17f79196dca
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-16 17:46:05 -0300

    EmployeeControllerRun

--- src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -1,18 +1,24 @@
 package com.example.demo.Controllers;
 
 import com.example.demo.Employees.Employee;
+import com.example.demo.Employees.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("Employees")
 public class EmployeeController
 {
+    @Autowired
+    private EmployeeRepository repository;
     @GetMapping
-    public void getAll()
+    public List<Employee> getAll()
     {
-        Employee employee
+        List<Employee> employeeList = repository.findAll();
+        return employeeList;
     }
 }
