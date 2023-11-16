88068aa7302a44841f53995f2eec5297c7354076
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-16 15:16:13 -0300

    CommitInit

---
+++ src/main/java/com/example/demo/EmployeeControlJavaSpringPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeControlJavaSpringPostgreSqlApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(EmployeeControlJavaSpringPostgreSqlApplication.class, args);
+	}
+}
