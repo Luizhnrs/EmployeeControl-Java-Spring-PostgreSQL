9e13aba8407162af8f6422352114cf8fb37ea5fb
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-17 17:25:40 -0300

    Lombok Add

--- src/main/java/com/example/demo/Employees/Employee.java
+++ src/main/java/com/example/demo/Employees/Employee.java
@@ -1,9 +1,17 @@
 package com.example.demo.Employees;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "Employees")
 @Entity(name = "Employees")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Employee
 {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
