48ea0e20d88ef03fddb226583899363c3adbb196
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-17 17:38:14 -0300

    PostImplementation

--- src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -4,9 +4,7 @@ import com.example.demo.Employees.Employee;
 import com.example.demo.Employees.EmployeeRepository;
 import com.example.demo.Employees.EmployeeResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +14,14 @@ public class EmployeeController
 {
     @Autowired
     private EmployeeRepository repository;
+    @PostMapping
+    public void SaveEmployee(@RequestBody EmployeeRequestDTO data){
+
+    }
     @GetMapping
     public List<EmployeeResponseDTO> getAll()
     {
-        List<Employee> employeeList = repository.findAll().stream();
+        List<EmployeeResponseDTO> employeeList = repository.findAll().stream().map(EmployeeResponseDTO::new).toList();
         return employeeList;
     }
 }
