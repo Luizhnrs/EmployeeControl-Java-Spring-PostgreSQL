24a860af35f3dfe021f2c5872a49fdc3f49acf16
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-17 13:46:04 -0300

    RepUpdate

--- src/main/java/com/example/demo/Employees/EmployeeRepository.java
+++ src/main/java/com/example/demo/Employees/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.Employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository <Employee, Long> {
+public interface EmployeeRepository extends JpaRepository <Employee, Long>
+{
 
 }
