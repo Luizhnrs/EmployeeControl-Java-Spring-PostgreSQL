d487e9dcdef649221fc091f28390a100c22c54e7
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-17 17:25:31 -0300

    RespondeDTO Getters

--- src/main/java/com/example/demo/Employees/EmployeeResponseDTO.java
+++ src/main/java/com/example/demo/Employees/EmployeeResponseDTO.java
@@ -3,5 +3,7 @@ package com.example.demo.Employees;
 import org.hibernate.tool.schema.spi.SchemaTruncator;
 
 public record EmployeeResponseDTO (long id, String name, String photo, Integer remuneration) {
-
+    public EmployeeResponseDTO (Employee employee){
+        this(employee.getId(), employee.getName(), employee.getPhoto(), employee.getRemuneration());
+    }
 }
