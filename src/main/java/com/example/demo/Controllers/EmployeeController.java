488adc8e26f33a2f3b9ad3163d1acea6994e6c30
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-17 17:09:34 -0300

    RespondeDTO Include

--- src/main/java/com/example/demo/Controllers/EmployeeController.java
+++ src/main/java/com/example/demo/Controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.demo.Controllers;
 
 import com.example.demo.Employees.Employee;
 import com.example.demo.Employees.EmployeeRepository;
+import com.example.demo.Employees.EmployeeResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,9 @@ public class EmployeeController
     @Autowired
     private EmployeeRepository repository;
     @GetMapping
-    public List<Employee> getAll()
+    public List<EmployeeResponseDTO> getAll()
     {
-        List<Employee> employeeList = repository.findAll();
+        List<Employee> employeeList = repository.findAll().stream();
         return employeeList;
     }
 }
