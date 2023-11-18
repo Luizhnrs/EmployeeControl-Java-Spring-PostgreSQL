9b00ae54a6c514308442f3a3b29ea552625c6518
Luizhnrs <Luizhnrsg@gmail.com>
2023-11-18 13:58:35 -0300

    Getters and Setters Implementation

--- src/main/java/com/example/demo/Employees/Employee.java
+++ src/main/java/com/example/demo/Employees/Employee.java
@@ -27,4 +27,36 @@ public class Employee
         this.name = data.name();
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Integer getRemuneration() {
+        return remuneration;
+    }
+
+    public void setRemuneration(Integer remuneration) {
+        this.remuneration = remuneration;
+    }
+
 }
